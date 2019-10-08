cd342878006e9ee2e6820390f8b04b1ccade7b32
Dennis <chatkhan@Chatkhans-MacBook-Air.local>
2019-10-08 11:40:48 -0700

    Added changes to proxy server

--- server/index.js
+++ server/index.js
@@ -1,15 +1,21 @@
 var express = require('express');
 var proxy = require('http-proxy-middleware');
+var path = require('path');
 
 var options = {
-    target: 'localhost:3000',
-    changeOrigin: true
+    target: 'localhost:4000',
+    changeOrigin: true,
+    router: {
+        'localhost:3000/api'
+    }
 };
 
 var exampleProxy = proxy(options);
 
 var app = express();
 var port = 4000;
+
+app.use(express.static(path.join(__dirname, '../client/public')))
 
 app.use('/api', exampleProxy);
 app.listen(port, () => console.log(`Listening on port ${port}!`));
